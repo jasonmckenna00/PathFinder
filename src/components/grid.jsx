baa3b2b9d6638b475017dfbf787d3f98c83cab62
Jason McKenna <jasonmckenna00@gmail.com>
2022-11-07 22:56:01 -0500

    start, finish, wall logic

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,14 +1,49 @@
-import { useEffect, useRef, useState } from "react"
+import { useState } from "react"
+import { act } from "react-dom/test-utils"
+const START = 'start'
+const PATH = 'path'
+const FINISH = 'finish'
+const EMPTY = 'empty'
+const VISITED = 'visited'
+const WALL = 'wall'
 
 const Grid = ({rows, columns}) => {
-  const [dataGrid,setDataGrid] = useState([...Array(rows)].map(_=>Array(columns).fill(0))) 
+  const [dataGrid,setDataGrid] = useState([...Array(rows)].map(_=>Array(columns).fill(EMPTY))) 
   const [totalClicked, setTotalClicked] = useState(0)
+  const [startPosition, setStartPosition] = useState([])
+  const [finishPosition, setFinishPosition] = useState([])
+  const [activeButton, setActiveButton] = useState('')
 
-  const handleClick = (r,c) =>{
+  const handleClick = (r,c,activeButton) =>{
     const newGrid = [...dataGrid]
-    newGrid[r][c] = 15
+
+   
+    const pos = JSON.stringify([r,c])
+    if (JSON.stringify(startPosition) === pos || JSON.stringify(finishPosition) === pos){
+      return
+    }
+
+
+    if (activeButton === START){
+      if (startPosition.length) newGrid[startPosition[0]][startPosition[1]] = EMPTY
+      setStartPosition([r,c])
+    } 
+    
+    else if (activeButton === FINISH){
+      if (finishPosition.length) newGrid[finishPosition[0]][finishPosition[1]] = EMPTY
+      setFinishPosition([r,c])
+    }
+
+    else if (activeButton === WALL){
+      //do something, not sure if needed. Logic can be elsewhere
+    }
+
+
+    newGrid[r][c] = activeButton
     setDataGrid(newGrid)
   }
+  
+
 
   const createGrid = () => {
     const newGrid = []
@@ -16,28 +51,56 @@ const Grid = ({rows, columns}) => {
     for (let c = 0; c < columns; c ++){
       const newRow = []
       for (let r = 0; r < rows; r++){
-        const val = dataGrid[r][c]
-        newRow.push(<Square key={r.toString()+ ','+c.toString()} value={val} handleClick={()=>handleClick(r,c)}/>)
+        const square = <Square 
+          key={r.toString()+ ','+c.toString()} 
+          state={dataGrid[r][c]} 
+          handleClick={()=>handleClick(r,c,activeButton)}
+        />
+        newRow.push(square)
       }
       newGrid.push(<div className="row" key={c}>{newRow}</div>)
     }
     return newGrid
   }
+
   const traverseGrid = () => {
-    // debugger
+
     let count = 0
     for(let r = 0; r < dataGrid.length; r++){
       for(let c = 0; c < dataGrid[0].length; c++){
         if (dataGrid[r][c] === 15) count += 1
-        
       }
     }
     setTotalClicked(count)
   }
 
+  const addItemToGrid = (item) => {
+    if (item === START) {
+      setActiveButton(START)
+    }
+    else if (item === FINISH) {
+      setActiveButton(FINISH)
+    }
+    else if (item === WALL){
+      setActiveButton(WALL)
+    }
+    // if (item === 'wall') return
+
+  }
+
+
+
+
+
   const disGrid = createGrid()
   return(
     <>
+      <h2>{activeButton}, Start: {startPosition}, Finish; {finishPosition}</h2>
+      <button onClick={() => addItemToGrid(START)}>Pick Start</button>
+      <button onClick={() => addItemToGrid(FINISH)}>Pick Finish</button>
+      <button onClick={() => addItemToGrid(WALL)}>Add Wall</button>
+      <button onClick={() => setActiveButton(EMPTY)}>Remove Wall</button>
+
       <div className="grid">{disGrid}</div>
       <h2>Total Clicked: {totalClicked}</h2>
       <button onClick={()=>traverseGrid()}>Traverse</button>
@@ -47,8 +110,23 @@ const Grid = ({rows, columns}) => {
 
 export default Grid
 
-const Square = ({value, handleClick}) => {
+const Square = ({state, handleClick}) => {
+  // state = 0, 1, 2, 3  0=blank(untouched), 1 = green(start/path), 2=checkered(finish), 3=gray(visited) 
+  // const getColor = () =>{
+  //   switch(state){
+  //     // case(1): return 'Green';
+  //     // case(2): return 'Checkered';
+  //     // case(3): return 'Blue';
+  //     // case(4): return 'Gray';
+  //     case(1): return 'path';
+  //     case(2): return 'finish';
+  //     case(3): return 'visited';
+  //     default: return 'white'
+  //   }
+  // }
+
+  // const color = getColor()
   return(
-    <div className="square" onClick={handleClick}>{value}</div>
+    <div className={`square ${state}`} onClick={handleClick}>{state}</div>
   )
 }
